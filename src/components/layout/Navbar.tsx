c47a4a8e1b9f96e474e1e0bd2096b93fa5f66e4b
HagOrMan <khagypi3@gmail.com>
2025-05-16 22:58:38 -0400

    [#18] fix: nav dropdown stays open when hovering between trigger and content

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -14,61 +14,96 @@ import { ThemeModeToggle } from '@/components/menu/ThemeMenu';
 import { NavbarItem } from '@/constant/layout/navItems';
 import { cn } from '@/lib/utils';
 import Link from 'next/link';
-import React from 'react';
+import React, { useState } from 'react';
 
 export type NavbarProps = {
   navbarItems: NavbarItem[];
 };
 
 export const Navbar = ({ navbarItems }: NavbarProps) => {
+  const [value, setValue] = useState(''); // represents the index as a string of the currently chosen navigation item
+
+  // Handles the value changing in the main navigation menu, which is really just an override so we manually control the value.
+  const handleValueChange = (newValue: string) => {
+    if (newValue !== '') {
+      setValue(newValue);
+    }
+  };
+
+  // Handles the pointer entering a navigation item.
+  const handlePointerEnter = async (index: number) => {
+    setValue(''); // Set the value to empty to "reset" it, which is important for radix to close the dropdown menu
+    if (`${index}` !== value) {
+      // the timeout is needed to close the dropdown menu, but we ensure we don't glitch it when we go from the dropdown back to the triggering nav item
+      await new Promise((resolve) => setTimeout(resolve, 1));
+    }
+    setValue(`${index}`);
+  };
+
   return (
-    <NavigationMenu className='sticky top-0 w-full max-w-full'>
+    <NavigationMenu
+      value={value}
+      onValueChange={handleValueChange}
+      className='sticky top-0 w-full max-w-full'
+      onPointerLeave={() => setValue('')}
+    >
       <NavigationMenuList>
         {navbarItems.map((item, index) => (
-          <NavigationMenuItem key={index}>
+          <NavigationMenuItem key={index} value={`${index}`}>
             {/*First we check if there are any dropdownItems. If yes, map down again.*/}
             {item.dropdownItems ? (
               // First give a trigger to open to dropdown menu with the title of the item
               <>
-                {item.link ? ( // Optionally has the trigger also be a link to a page
-                  <Link href={item.link}>
-                    <NavigationMenuTrigger className='hover:bg-lush-800 hover:text-accent-foreground data-[state=open]:bg-lush-800/70 rounded-md'>
+                <div onPointerEnter={() => handlePointerEnter(index)}>
+                  {item.link ? (
+                    // Optionally has the trigger also be a link to a page
+                    <Link href={item.link}>
+                      <NavigationMenuTrigger className='rounded-md hover:bg-lush-800 hover:text-accent-foreground data-[state=open]:bg-lush-800/70'>
+                        {item.title}
+                      </NavigationMenuTrigger>
+                    </Link>
+                  ) : (
+                    <NavigationMenuTrigger className='rounded-md hover:bg-lush-800 hover:text-accent-foreground'>
                       {item.title}
                     </NavigationMenuTrigger>
-                  </Link>
-                ) : (
-                  <NavigationMenuTrigger className='hover:bg-lush-800 hover:text-accent-foreground rounded-md'>
-                    {item.title}
-                  </NavigationMenuTrigger>
-                )}
+                  )}
+                </div>
+
                 {/* Next we include all the content needed inside the dropdown */}
-                <NavigationMenuContent className='from-lush-200 to-breeze-400 !bg-gradient-to-b focus:shadow-md'>
-                  <ul className='grid w-[400px] gap-3 p-4 md:w-[500px] lg:w-[600px]'>
-                    {item.dropdownItems.map((dropdownItem, index) => (
-                      <div key={dropdownItem.title + index}>
-                        <ListItem
-                          title={dropdownItem.title}
-                          href={dropdownItem.link}
-                        >
-                          {dropdownItem.description || ''}
-                        </ListItem>
-                      </div>
-                    ))}
-                  </ul>
+                <NavigationMenuContent className='!bg-gradient-to-b from-lush-200 to-breeze-400 focus:shadow-md'>
+                  <div
+                    className='relative'
+                    onPointerEnter={() => setValue(`${index}`)}
+                  >
+                    <ul className='grid w-[400px] gap-3 p-4 md:w-[500px] lg:w-[600px]'>
+                      {item.dropdownItems.map((dropdownItem, index) => (
+                        <div key={dropdownItem.title + index}>
+                          <ListItem
+                            title={dropdownItem.title}
+                            href={dropdownItem.link}
+                          >
+                            {dropdownItem.description || ''}
+                          </ListItem>
+                        </div>
+                      ))}
+                    </ul>
+                  </div>
                 </NavigationMenuContent>
               </>
             ) : (
               // This occurs if there are no dropdown items and there just needs to be a button in the navbar that links somewhere
-              <Link href={item.link!} legacyBehavior passHref>
-                <NavigationMenuLink
-                  className={cn(
-                    navigationMenuTriggerStyle(),
-                    'hover:bg-lush-800 hover:text-accent-foreground rounded-md',
-                  )}
-                >
-                  {item.title}
-                </NavigationMenuLink>
-              </Link>
+              <div onPointerEnter={() => handlePointerEnter(index)}>
+                <Link href={item.link!} legacyBehavior passHref>
+                  <NavigationMenuLink
+                    className={cn(
+                      navigationMenuTriggerStyle(),
+                      'rounded-md hover:bg-lush-800 hover:text-accent-foreground',
+                    )}
+                  >
+                    {item.title}
+                  </NavigationMenuLink>
+                </Link>
+              </div>
             )}
           </NavigationMenuItem>
         ))}
@@ -98,13 +133,13 @@ const ListItem = React.forwardRef<
         <a
           ref={ref}
           className={cn(
-            'hover:bg-accent hover:text-accent-foreground block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors',
+            'block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground',
             className,
           )}
           {...props}
         >
           <div className='text-sm font-bold leading-none'>{title}</div>
-          <p className='text-muted-foreground line-clamp-2 text-sm leading-snug'>
+          <p className='line-clamp-2 text-sm leading-snug text-muted-foreground'>
             {children}
           </p>
         </a>
