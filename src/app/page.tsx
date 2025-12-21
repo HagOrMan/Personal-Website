f4f53a5345e5f0cce8c21627ea8e67223c7588d9
HagOrMan <86536365+HagOrMan@users.noreply.github.com>
2025-12-21 17:36:19 -0500

    fix: width of particles too big for screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ import { GlitchTextCycle } from '@/components/text/GlitchTextCycle';
 export default function Home() {
   return (
     <div className='bg-background grid grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-(family-name:--font-geist-sans) sm:p-20'>
-      <div className='absolute top-0 left-0 h-screen w-screen'>
+      <div className='absolute top-0 left-0 h-screen w-full'>
         {/* <ElectricShockBackground /> */}
         <OceanScene />
         {/* gradient to blend into page below */}
