6d9aeec9e778eb4ebb24cec70c61a803163a858e
HagOrMan <86536365+HagOrMan@users.noreply.github.com>
2025-12-23 15:03:28 -0500

    [#33] feat: reorganize main page for wave background to be in first section, maintaining scrolling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,40 +23,52 @@ export default function Home() {
   return (
     <div
       ref={containerRef}
-      className='bg-background grid grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-(family-name:--font-geist-sans) sm:p-20'
+      className='bg-background min-h-screen w-full overflow-x-hidden font-(family-name:--font-geist-sans)'
     >
-      {/* Container for ocean background at top of page */}
-      <div className='absolute top-0 left-0 h-screen w-full'>
-        {/* <ElectricShockBackground /> */}
-        <OceanScene />
-        {/* gradient to blend into page below */}
-        <div className='from-background absolute bottom-0 left-0 h-32 w-full bg-gradient-to-t to-transparent' />
-      </div>
+      <main className='relative z-10 flex w-full flex-col'>
+        {/* The intro page, with a welcome message */}
+        <section className='relative flex h-screen w-full flex-col items-center p-4'>
+          {/* Container for ocean background at top of page */}
+          <div className='absolute inset-0 z-0 -translate-y-6'>
+            {/* <ElectricShockBackground /> */}
+            <OceanScene />
+            {/* gradient to blend into page below */}
+            <div className='from-background absolute bottom-0 left-0 h-32 w-full bg-gradient-to-t to-transparent' />
+          </div>
 
-      {/* Main content, centered on page */}
-      <main className='relative z-10 row-start-2 flex flex-col items-center gap-8 sm:items-start'>
-        {/* Liquid glass card that starts invisible and appears as the text inside moves left on page. */}
-        <LiquidGlassCard
-          alpha={glassOpacity}
-          style={{ x: xPosition }}
-          className='-translate-x-8 -translate-y-8'
-          contentClassName='relative row-start-2 flex flex-col items-start gap-8'
-        >
-          <h1 className='text-primary-rgb-700 text-4xl font-bold tracking-wide'>
-            Hey! I&apos;m Kyle
-          </h1>
-          <GlitchTextCycle
-            words={['Developer', 'Innovator', 'Creator']}
-            className='text-primary-rgb-600'
-          />
-        </LiquidGlassCard>
+          {/* Liquid glass card that starts invisible and appears as the text inside moves left on page. */}
+          <div className='pointer-events-none relative z-10 mt-30 flex flex-col items-center gap-8'></div>
+          <LiquidGlassCard
+            alpha={glassOpacity}
+            style={{ x: xPosition }}
+            className='pointer-events-auto -translate-x-16'
+            contentClassName='relative row-start-2 flex flex-col items-start gap-8'
+          >
+            <h1 className='text-primary-rgb-700 text-4xl font-bold tracking-wide'>
+              Hey! I&apos;m Kyle
+            </h1>
+            <GlitchTextCycle
+              words={['Developer', 'Innovator', 'Creator']}
+              className='text-primary-rgb-600'
+            />
+          </LiquidGlassCard>
+        </section>
 
-        <div className='relative mt-10'>
-          <h1 className='hidden'>
-            Deep dive into my skills and passion and experience
-          </h1>
-          <div className='h-[500px]'>lots of info</div>
-        </div>
+        {/* Experience */}
+        <section className='mx-auto flex min-h-screen w-full max-w-5xl flex-col items-start justify-center p-10'>
+          <h2 className='mb-8 text-3xl font-bold'>EXPERIENCE</h2>
+          <div className='h-[500px] w-full rounded-xl border border-black/10 p-6 dark:border-white/10'>
+            <p>Co-op</p>
+          </div>
+        </section>
+
+        {/* Projects */}
+        <section className='flex min-h-screen w-full flex-col items-end justify-center p-10'>
+          <div className='w-1/2'>
+            <h2 className='mb-4 text-3xl font-bold'>PROJECTS</h2>
+            <p>This?</p>
+          </div>
+        </section>
       </main>
     </div>
   );
