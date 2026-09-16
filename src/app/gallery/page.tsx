23fbd7b69924347689c8a433ade7bfe824a27476
HagOrMan <86536365+HagOrMan@users.noreply.github.com>
2026-09-15 22:31:44 -0400

    feat: add gallery code to show off photos ive taken

---
+++ src/app/gallery/page.tsx
@@ -0,0 +1,25 @@
+import { WaveSpray } from '@/components/animated-fun/Wavespray';
+import PhotoWall from '@/components/gallery/PhotoWall';
+import { PageHeader } from '@/components/layout/PageHeader';
+
+export default function Gallery() {
+  return (
+    <main className='bg-background page-shell'>
+      <PageHeader
+        title='Gallery'
+        description='A collection of photos I took over the years. What catches my eye or I want to remember.'
+        decoration={
+          <div className='size-24 overflow-hidden rounded-2xl md:size-28'>
+            <WaveSpray
+              colorStart='--tw-color-lush-500'
+              colorEnd='--tw-color-nebula-400'
+            />
+          </div>
+        }
+        fadeDecoration={true}
+      />
+
+      <PhotoWall />
+    </main>
+  );
+}
